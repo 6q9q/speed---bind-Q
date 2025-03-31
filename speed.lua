b687fae1171ebb02b595a26a46eb614b10d25e6a
6q9q <vscodeidkprofile@gmail.com>
2025-03-31 16:23:48 +0500

    Update speed.lua

---
+++ speed.lua
@@ -0,0 +1,21 @@
+local UserInputService = game:GetService("UserInputService")
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local humanoid = char:FindFirstChildOfClass("Humanoid")
+
+local isSpeedBoosted = false
+local normalSpeed = 16
+local boostedSpeed = 150
+
+UserInputService.InputBegan:Connect(function(input, gameProcessed)
+    if gameProcessed then return end -- Игнорируем, если игрок в чате или меню
+
+    if input.KeyCode == Enum.KeyCode.Q then
+        if humanoid then
+            isSpeedBoosted = not isSpeedBoosted
+            humanoid.WalkSpeed = isSpeedBoosted and boostedSpeed or normalSpeed
+        end
+    end
+end)
